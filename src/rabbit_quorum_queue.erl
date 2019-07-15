475135796dadec34a15c5d5c6febdf5281929d34
kjnilsson <knilsson@pivotal.io>
2019-07-15 12:33:59 +0100

    Queue type: dequeue/basic_get

--- src/rabbit_quorum_queue.erl
+++ src/rabbit_quorum_queue.erl
@@ -21,7 +21,7 @@
 -export([init/1, handle_event/2]).
 -export([recover/1, stop/1, delete/4, delete_immediately/2]).
 -export([info/1, info/2, stat/1, infos/1]).
--export([settle/4, reject/4, basic_get/4, consume/3, cancel/6]).
+-export([settle/4, reject/4, dequeue/4, consume/3, cancel/6]).
 -export([credit/4]).
 -export([purge/1]).
 -export([stateless_deliver/2, deliver/3, deliver/2]).
@@ -544,14 +544,12 @@ reject(CTag, false, MsgIds, QState) ->
 credit(CTag, Credit, Drain, QState) ->
     rabbit_fifo_client:credit(quorum_ctag(CTag), Credit, Drain, QState).
 
--spec basic_get(amqqueue:amqqueue(), NoAck :: boolean(), rabbit_types:ctag(),
-                rabbit_fifo_client:state()) ->
-    {'ok', 'empty', rabbit_fifo_client:state()} |
-    {'ok', QLen :: non_neg_integer(), qmsg(), rabbit_fifo_client:state()} |
+-spec dequeue(NoAck :: boolean(), pid(),
+              rabbit_types:ctag(), rabbit_fifo_client:state()) ->
+    {empty, rabbit_fifo_client:state()} |
+    {ok, QLen :: non_neg_integer(), qmsg(), rabbit_fifo_client:state()} |
     {error, timeout | term()}.
-basic_get(Q, NoAck, CTag0, QState0) when ?amqqueue_is_quorum(Q) ->
-    QName = amqqueue:get_name(Q),
-    Id = amqqueue:get_pid(Q),
+dequeue(NoAck, _LimiterPid, CTag0, QState0) ->
     CTag = quorum_ctag(CTag0),
     Settlement = case NoAck of
                      true ->
@@ -559,27 +557,7 @@ basic_get(Q, NoAck, CTag0, QState0) when ?amqqueue_is_quorum(Q) ->
                      false ->
                          unsettled
                  end,
-    case rabbit_fifo_client:dequeue(CTag, Settlement, QState0) of
-        {ok, empty, QState} ->
-            {ok, empty, QState};
-        {ok, {{MsgId, {MsgHeader, Msg0}}, MsgsReady}, QState} ->
-            Count = case MsgHeader of
-                        #{delivery_count := C} -> C;
-                       _ -> 0
-                    end,
-            IsDelivered = Count > 0,
-            Msg = rabbit_basic:add_header(<<"x-delivery-count">>, long, Count, Msg0),
-            {ok, MsgsReady, {QName, Id, MsgId, IsDelivered, Msg}, QState};
-        {error, unsupported} ->
-            rabbit_misc:protocol_error(
-              resource_locked,
-              "cannot obtain access to locked ~s. basic.get operations are not supported by quorum queues with single active consumer",
-              [rabbit_misc:rs(QName)]);
-        {error, _} = Err ->
-            Err;
-        {timeout, _} ->
-            {error, timeout}
-    end.
+    rabbit_fifo_client:dequeue(CTag, Settlement, QState0).
 
 -spec consume(amqqueue:amqqueue(),
               rabbit_queue_type:consume_spec(),
@@ -589,7 +567,7 @@ basic_get(Q, NoAck, CTag0, QState0) when ?amqqueue_is_quorum(Q) ->
 consume(Q, #{limiter_active := true}, _State)
   when ?amqqueue_is_quorum(Q) ->
     {error, global_qos_not_supported_for_queue_type};
-consume(Q, Spec, State0) when ?amqqueue_is_quorum(Q) ->
+consume(Q, Spec, QState0) when ?amqqueue_is_quorum(Q) ->
     #{no_ack := NoAck,
       channel_pid := ChPid,
       prefetch_count := ConsumerPrefetchCount,
